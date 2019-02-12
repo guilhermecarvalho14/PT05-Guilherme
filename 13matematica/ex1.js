db1905500214c609107e002552e05d5b9115414a
guilhermecarvalho14 <11420@aebenfica.org>
2019-02-12 12:58:40 +0000

    added ";"

--- 13matematica/ex1.js
+++ 13matematica/ex1.js
@@ -1,7 +1,7 @@
 // Add 2 numbers script
 
-var num1, num2, sum
-num1 = prompt("Enter first number")
-num2 = prompt("Enter second number")
-sum = parseInt(num1) + parseInt(num2) // "+" means "add"
-alert("Sum = " + sum)  // "+" means combine into a string
+var num1, num2, sum;
+num1 = prompt("Enter first number");
+num2 = prompt("Enter second number");
+sum = parseInt(num1) + parseInt(num2); // "+" means "add"
+alert("Sum = " + sum);  // "+" means combine into a string
